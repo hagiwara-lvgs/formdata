c6f5f79850e35fb3c78fcff283ddc2b56263565a
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2020-02-14 20:35:56 +0900

    add resize

--- resources/js/upload.js
+++ resources/js/upload.js
@@ -13,24 +13,49 @@ fileInput.on('change', (e) => {
     }).then((response) => {
         let key;
         let formData = new FormData();
-
+        
         for (key in response.data) {
             if (response.data.hasOwnProperty(key)) {
                 formData.append(key, response.data[key]);
             }
         }
-        formData.append('file', file);
-        return $.ajax({
-            url: response.url,
-            type: 'POST',
-            data: formData,
-            dataType: 'xml',
-            processData: false,
-            contentType: false,
-        }).catch((error) => {
-            console.log('署名作成エラー');
-            console.log(error);
-        });
+
+        // リサイズ
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            let image = new Image();
+            image.onload = () => {
+                console.log(1)
+                let canvas = document.createElement('canvas');
+                let ctx = canvas.getContext('2d');
+                [canvas.width, canvas.height] = [image.width, image.height];
+                ctx.drawImage(image, 0, 0);
+                let base64 = canvas.toDataURL(file.type);
+                let bin = atob(base64.replace(/^.*,/, ''));
+                let buffer = new Uint8Array(bin.length);
+                for (let i = 0; i < bin.length; i++) {
+                    buffer[i] = bin.charCodeAt(i);
+                }
+                let blob = new Blob([buffer.buffer], {
+                    type: file.type
+                });
+                console.log(blob)
+                formData.append('file', blob);
+                return $.ajax({
+                    url: response.url,
+                    type: 'POST',
+                    data: formData,
+                    dataType: 'xml',
+                    processData: false,
+                    contentType: false,
+                }).catch((error) => {
+                    console.log('署名作成エラー');
+                    console.log(error);
+                });
+            };
+            image.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
     }).then((response) => {
         let body = $('body');
         let img = document.createElement('img');
